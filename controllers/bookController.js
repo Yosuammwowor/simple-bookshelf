cc7e5f0bf1cf92e7f739a3e7d243b81dc0e778ed
Yosua <yosuammwowor@gmail.com>
2026-03-26 19:46:22 +0800

    feat: get specific book by id through GET /books/:id endpoint

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,4 +4,16 @@ async function controllerGetAllBooks() {
   return await getAllBooks();
 }
 
-export { controllerGetAllBooks };
+async function controllerGetBookById(id) {
+  const res = await getAllBooks();
+
+  const bookIsValid = res.data.find((book) => book.id === id);
+
+  if (!bookIsValid) {
+    return { status: "fail", message: "Invalid, no book id match" };
+  }
+
+  return { status: "success", data: bookIsValid };
+}
+
+export { controllerGetAllBooks, controllerGetBookById };
--- routes/routeHandlers.js
+++ routes/routeHandlers.js
@@ -1,9 +1,20 @@
-import { controllerGetAllBooks } from "../controllers/bookController.js";
+import {
+  controllerGetAllBooks,
+  controllerGetBookById,
+} from "../controllers/bookController.js";
 import express from "express";
 const router = express.Router();
 
 router.get("/books", async (req, res) => {
   res.send(await controllerGetAllBooks());
+});
+
+router.get("/books/:id", async (req, res) => {
+  const result = await controllerGetBookById(req.params.id);
+
+  if (result.status === "fail") res.status(404);
+
+  return res.send(result);
 });
 
 export { router };
